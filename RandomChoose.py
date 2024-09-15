a6262d993e77b610a22faf1c61ebb8df32ddc5c3
“zhangxuan2011” <zx20110412@outlook.com>
2024-09-15 15:00:48 +0800

    Updated RandomChoose to v2.0

--- RandomChoose.py
+++ RandomChoose.py
@@ -1,11 +1,12 @@
-import random
-import os
+from random import randint as kernel
+import tkinter
 
 
-def kernel(minNum, maxNum):
-    return random.randint(minNum, maxNum)
+root = tkinter.Tk()
+root.title("Random Choose Main Window")
+root.geometry("850x500")
 minNum = 1
 maxNum = 45
-print(f"选中了：{kernel(minNum, maxNum)}")
-print("press any key to continue......")
-os.system("pause > nul")
+info = tkinter.Label(root, text=f"选中了：{kernel(minNum, maxNum)}号", font=("微软雅黑", 40))
+info.place(x=200, y=200)
+root.mainloop()
