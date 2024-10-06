b4df04b86f2db29d621645e35ba30ecddc141c83
zhangxuan2011 <zx20110412@outlook.com>
2024-10-06 15:01:19 +0800

    MAKE CODE DIFF

---
+++ mkcodediff.py
@@ -0,0 +1,25 @@
+import time
+import os
+import random
+
+
+class mkcodediff:
+	def __init__(self):
+		h = 'h'
+		for i in range(1):
+			e = 'e'
+			if 1 == 1:
+				random.randint(0, 0)
+				pout = print
+				l = 'l'
+				while True:
+					time.sleep(0)
+					o = 'o'
+					break
+				merge1 = list()
+			merge1 = [h,e,l,l,o]
+			
+			pout(mergestr1)
+			
+if __name__ == '__main__':
+	mkcodediff()					
