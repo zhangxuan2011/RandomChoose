8bcf3e88fe85c1eaa73ee35d673b997e8ded6f58
zhangxuan2011 <zx20110412@outlook.com>
2024-10-07 18:07:52 +0800

    :)

--- mkcodediff.py
+++ mkcodediff.py
@@ -1,3 +1,9 @@
+'''
+How can we PRINT a hello world?
+This is a example
+'''
+
+
 import time
 import os
 import random
@@ -15,11 +21,28 @@ class mkcodediff:
 				while True:
 					time.sleep(0)
 					o = 'o'
+					symbol1 = ','
 					break
 				merge1 = list()
-			merge1 = [h,e,l,l,o]
-			
-			pout(mergestr1)
+			merge1 = [h,e,l,l,o,symbol1,]	#Merge code
+			mergestr1 = ''.join(merge1)
+			for i in range(1):
+				if not 1 == 2:
+					random.randint(114514, 1919810)	# delicious code
+					ls = list
+					w = 'w'
+					while True:
+						r = 'r'
+						if 6 == 6:	# Good code
+							d = 'd'
+							if not 114514 == 1919810:
+								symbol2 = '!'
+						merge2 = ls()
+						merge2 = [w,o,r,l,d,symbol2]
+						mergestr2 = ''.join(merge2)
+						break	# break code
+					merge = mergestr1 + mergestr2
+			pout(merge)
 			
 if __name__ == '__main__':
 	mkcodediff()					
