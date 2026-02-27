ad4eebe5e0de19d157f50fe2e2a156cdfeabb9ad
zhangxuan2011 <zx20110412@outlook.com>
2026-02-27 18:57:38 +0800

    chore: Added the android gen manifest  - Generated the android file through `npm run tauri android init`

---
+++ src-tauri/gen/android/app/src/main/java/com/zhangxuan2011/random_choose/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zhangxuan2011.random_choose
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
